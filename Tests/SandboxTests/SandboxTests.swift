cd2725e3d83901ca4ae23354d884630830dba9ff
tdrk18 <tdrk18@gmail.com>
2025-02-06 00:47:19 +0900

    Add failing test

--- Tests/SandboxTests/SandboxTests.swift
+++ Tests/SandboxTests/SandboxTests.swift
@@ -4,3 +4,8 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+@Test func failed() async throws {
+    // This test will fail.
+    #expect(1 == 2)
+}
